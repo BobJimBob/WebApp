473dba190d3ad074774632703dcd0c1625230eb2
BobJimBob <58898597+BobJimBob@users.noreply.github.com>
2020-02-10 15:32:37 +0800

    Add files via upload

--- Forms.py
+++ Forms.py
@@ -2,6 +2,9 @@ from flask_wtf import *
 from wtforms import *
 from wtforms.fields.html5 import *
 from wtforms.validators import *
+from flask_wtf.file import *
+
+
 
 class FeedbackForm(Form):
  fullName = StringField('Full Name', [validators.Length(min=1,max=150), validators.DataRequired()])
@@ -64,3 +67,19 @@ class QuestionForm(FlaskForm):
 class ResetChangeForm(FlaskForm):
     newpassword = PasswordField('New password', validators=[InputRequired(), Length(min=8, max=80)])
     confirmpassword = PasswordField('Confirm password', validators=[InputRequired(), Length(min=8, max=80)])
+
+'''
+eugenes shit
+'''
+class CreateProductForm(Form):
+    productName = StringField('Product Name ',[validators.Length(min=1, max=150), validators.DataRequired()])
+    author = StringField('Author', [validators.Length(min=1, max=150), validators.DataRequired()])
+    publisher = StringField('Publisher', [validators.Length(min=1, max=150), validators.DataRequired()])
+    genre = RadioField('Genre', choices=[('Children', 'Children'), ('Crime', 'Crime'), ('Fiction','Fiction'),('Manga','Manga')], default='Fiction')
+    price =  StringField('Prices ',[validators.Length(min=1, max=150),validators.input_required()])
+    remarks = TextAreaField('Product Description', [validators.Optional()])
+    stocks = IntegerField("Stocks ",[validators.input_required()])
+
+    image_1 = FileField("Image 1 ", validators=[FileAllowed(["jpg", "png", "jpgeg","webp"])])
+    image_2 = FileField("Image 2 ", validators=[FileAllowed(["jpg", "png", "jpgeg","webp"])])
+    image_3 = FileField("Image 3 ", validators=[FileAllowed(["jpg", "png", "jpgeg","webp"])])
--- cartentry.py
+++ cartentry.py
@@ -107,6 +107,167 @@ class orderinfoC:
     def set_status(self, status):
         self.__status = status
 
+class Order:
+    def __init__(self, orderID, date_sent, date_received, delivery_types, delivery_status, admin_remarks,
+                 user_remarks, delivery_remarks, verification_code):
+        self.__orderID = orderID
+        self.__date_sent = date_sent
+        self.__date_received = date_received
+        self.__delivery_types = delivery_types
+        self.__delivery_status = delivery_status
+        self.__admin_remarks = admin_remarks
+        self.__user_remarks = user_remarks
+        self.__delivery_remarks = delivery_remarks
+        self.__verification_code = verification_code
+
+    def get_orderID(self):
+        return self.__orderID
+
+    def get_date_sent(self):
+        return self.__date_sent
+
+    def get_date_received(self):
+        return self.__date_received
+
+    def get_delivery_types(self):
+        return self.__delivery_types
+
+    def get_delivery_status(self):
+        return self.__delivery_status
+
+    def get_admin_remarks(self):
+        return self.__admin_remarks
+
+    def get_user_remarks(self):
+        return self.__user_remarks
+
+    def get_delivery_remarks(self):
+        return self.__delivery_remarks
+
+    def get_verification(self):
+        return self.__verification_code
+
+    def set_orderID(self, orderID):
+        self.__orderID = orderID
+
+    def set_date_sent(self, date_sent):
+        self.__date_sent = date_sent
+
+    def set_date_received(self, date_received):
+        self.__date_received = date_received
+
+    def set_delivery_types(self, delivery_types):
+        self.__delivery_types = delivery_types
+
+    def set_delivery_status(self, delivery_status):
+        self.__delivery_status = delivery_status
+
+    def set_admin_remarks(self, admin_remarks):
+        self.__admin_remarks = admin_remarks
+
+    def set_user_remarks(self, user_remarks):
+        self.__user_remarks = user_remarks
+
+    def set_delivery_remarks(self, delivery_remarks):
+        self.__delivery_remarks = delivery_remarks
+
+    def set_verification(self, verification_code):
+        self.__verification_code = verification_code
+
+
+
+class Product:
+    countID = 0
+    def __init__(self,productName,author,publisher,genre,price,stocks,remarks,image_1,image_2,image_3):
+        Product.countID +=1
+        productuuid = uuid.uuid4()
+        self.__userID = str(productuuid)
+        self.__productName = productName
+        self.__author = author
+        self.__publisher = publisher
+        self. __genre = genre
+        self.__price = price
+        self.__stocks = stocks
+        self.__remarks = remarks
+        self.__image_1 = image_1
+        self.__image_2 = image_2
+        self.__image_3 = image_3
+
+
+
+
+    def get_userID(self):
+        return self.__userID
+
+    def get_productName(self):
+        return self.__productName
+
+    def get_author(self):
+        return self.__author
+
+
+    def get_publisher(self):
+        return self.__publisher
+
+    def get_genre(self):
+        return self.__genre
+
+    def get_price(self):
+        return self.__price
+
+
+    def get_remarks(self):
+        return self.__remarks
+
+
+    def get_stocks(self):
+        return self.__stocks
+
+
+    def get_image_1(self, ):
+        return self.__image_1
+
+    def get_image_2(self,):
+        return self .__image_2
+
+    def get_image_3(self, ):
+        return self.__image_3
+
+
+    def set_userID(self,userID):
+        self.__userID = userID
+
+    def set_productName(self,productName):
+        self.__productName = productName
+
+    def set_author(self,author):
+        self.__author = author
+
+    def set_genre(self,genre):
+         self.__genre = genre
+
+
+    def set_publisher(self,publisher):
+        self.__publisher = publisher
+
+    def set_price(self,price):
+        self.__price = price
+
+    def set_remarks(self,remarks):
+        self.__remarks = remarks
+
+    def set_stocks(self,stocks):
+        self.__stocks = stocks
+
+    def set_image_1(self,image_1):
+        self.__image_1 = image_1
+
+    def set_image_2(self, image_2):
+        self.__image_2 = image_2
+
+    def set_image_3(self, image_3):
+        self.__image_3 = image_3
+
 
 inventory = shelve.open('inventory.db', 'c')
 
@@ -137,7 +298,7 @@ def clear_inventory():
 
 def initdb():
     clear_inventory()
-    create_new_dbItem( 'book 1', '/static/images/img1.jpg', 20, 50)
-    create_new_dbItem( 'Book 2', '/static/images/img2.jpg', 30,43)
-    create_new_dbItem( 'Book 3', '/static/images/img3.jpg', 40,60)
+    create_new_dbItem('book 1', '/static/images/img1.jpg', 20, 50)
+    create_new_dbItem('Book 2', '/static/images/img2.jpg', 30, 43)
+    create_new_dbItem('Book 3', '/static/images/img3.jpg', 40, 60)
     create_new_dbItem('Book 4', '/static/images/img4.jpg', 50, 70)
